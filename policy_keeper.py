3d7861086ec14383ec293543d65713987c193503
Jozsef Kovacs <jozsef.kovacs@sztaki.mta.hu>
2018-06-05 11:33:02 +0200

    fix(policy): fixing bug in resolve stack name

--- policy_keeper.py
+++ policy_keeper.py
@@ -19,9 +19,10 @@ log = None
 def resolve_queries(policy_yaml):
   stack = dict()
   stack['stack'] = yaml.safe_load(policy_yaml).get('stack','undefined_stack_name')
-  template = jinja2.Template(policy_yaml)
+  env = jinja2.Environment(undefined=jinja2.DebugUndefined)
+  template = env.from_string(policy_yaml)
   policy_yaml = template.render(stack)
-  
+
   values = yaml.safe_load(policy_yaml).get('data',dict()).get('constants',dict())
   log.info('values: {0}'.format(values))
   template = jinja2.Template(policy_yaml)
