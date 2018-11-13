b26d03c802f3bcf195d55fe5fac8c37af2a9566a
jaydesl <jay.deslauriers@gmail.com>
2018-11-13 11:29:24 +0000

    Disable network changes via handler

--- handle_k8s.py
+++ handle_k8s.py
@@ -1,6 +1,5 @@
 import kubernetes.client
 import kubernetes.config
-import docker
 import logging
 import pk_config
 import time
@@ -62,48 +61,46 @@ def query_docker_service_replicas(endpoint,service_name):
 def query_service_network(endpoint, stack_name, service_name):
   id = None
   log=logging.getLogger('pk_docker')
-  client = docker.DockerClient(base_url=endpoint)
-  full_service_name = stack_name + "_" + service_name
-  if pk_config.simulate():
-    return None
-  service_list = client.services.list()
-  i = 0
-  while i < len(service_list) and service_list[i].name != full_service_name:
-    i += 1
-  if i < len(service_list) and service_list[i].name == full_service_name:
-    if len(service_list[i].attrs.get("Spec").get("TaskTemplate").get("Networks")) == 1:
-      id = service_list[i].attrs.get("Spec").get("TaskTemplate").get("Networks")[0].get("Target")
-      log.debug('Docker service "{0}" in stack "{1}" is connected to network "{2}" with id "{3}".'
-		 .format(service_name, stack_name, client.networks.get(id).name),str(id))
-    else:
-      log.warning('Docker service "{0}" is connected to more than one network.'.format(full_service_name))
-  else:
-    log.warning('Docker service "{0}" is not found in stack "{1}".'.format(service_name,stack_name))
+  #client = docker.DockerClient(base_url=endpoint)
+  #full_service_name = stack_name + "_" + service_name
+  #if pk_config.simulate():
+    #return None
+  #service_list = client.services.list()
+  #i = 0
+  #while i < len(service_list) and service_list[i].name != full_service_name:
+    #i += 1
+  #if i < len(service_list) and service_list[i].name == full_service_name:
+    #if len(service_list[i].attrs.get("Spec").get("TaskTemplate").get("Networks")) == 1:
+      #id = service_list[i].attrs.get("Spec").get("TaskTemplate").get("Networks")[0].get("Target")
+      #log.debug('Docker service "{0}" in stack "{1}" is connected to network "{2}" with id "{3}".'
+		 #.format(service_name, stack_name, client.networks.get(id).name),str(id))
+    #else:
+      #log.warning('Docker service "{0}" is connected to more than one network.'.format(full_service_name))
+  #else:
+  log.warning('Docker networking not supported in kubernetes, cannot query')
   return id
 
 
 def attach_container_to_network(endpoint, container, network_id):
   log=logging.getLogger('pk_docker')
-  client = docker.DockerClient(base_url=endpoint)
-  network = client.networks.get(network_id)
-  if client.containers.get(container).status == "running":
-    network.connect(container)
-    log.info('Container "{0}" is connected to network "{1}".'.format(container, network.name))
-  else:
-    log.info('Container "{0}" cannot be connected to network "{1}" as it is not running.'
-	     .format(container,network.name))
+  #client = docker.DockerClient(base_url=endpoint)
+  #network = client.networks.get(network_id)
+  #if client.containers.get(container).status == "running":
+    #network.connect(container)
+    #log.info('Container "{0}" is connected to network "{1}".'.format(container, network.name))
+  #else:
+  log.warning('Docker networking not supported in kubernetes, attach container failed')
   return
 
 def detach_container_from_network(endpoint, container, network_id):
   log=logging.getLogger('pk_docker')
-  client = docker.DockerClient(base_url=client_address, version=client_version)
-  network = client.networks.get(network_id)
-  if client.containers.get(container).status == "running":
-    network.disconnect(container)
-    log.info('Container "{0}" is disconnected from network "{1}".'.format(container, network.name))
-  else:
-    log.info('Container "{0}" cannot be disconnected from network "{1}" as it is not running.'
-	     .format(container,network.name))
+  #client = docker.DockerClient(base_url=client_address, version=client_version)
+  #network = client.networks.get(network_id)
+  #if client.containers.get(container).status == "running":
+    #network.disconnect(container)
+    #log.info('Container "{0}" is disconnected from network "{1}".'.format(container, network.name))
+  #else:
+  log.warning('Docker networking not supported in kubernetes, detach container failed')
   return
 
 down_nodes_stored={}
