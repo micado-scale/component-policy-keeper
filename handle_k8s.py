8f912b208ba4791bb0a54914b545c81d78f4638e
jaydesl <jay.deslauriers@gmail.com>
2020-03-31 09:27:46 +0100

    Don't drop "not ready" nodes

--- handle_k8s.py
+++ handle_k8s.py
@@ -28,8 +28,16 @@ def query_list_of_nodes(endpoint,worker_name='micado-worker',status='ready'):
       query = pykube.Node.objects(kube).filter(selector={"micado.eu/node_type__in": {worker_name}})
       nodes = [x for x in query if "taints" not in x.obj["spec"]]
     elif status=='down':
+      nodes = []
       worker_nodes = [x for x in pykube.Node.objects(kube) if MASTER not in x.labels]
-      nodes = [x for x in worker_nodes if "taints" in x.obj["spec"]]
+      for node in worker_nodes:
+        ready_condition = [
+          x.iteritems()
+          for x in node.obj["status"]["conditions"]
+          if x.get("type") == "Ready"
+        ][0]
+        if ("status", "Unknown") in ready_condition:
+          nodes.append(node)
     for n in nodes:
       a = {}
       n.reload()
@@ -94,7 +102,7 @@ def remove_node(endpoint,id):
   return
 
 def down_nodes_cleanup_by_list(stored, actual):
-  setStored = { v['ID'] for k,v in stored.items() }
+  setStored = { v['ID'] for k,v in stored.iteritems() }
   setActual = { x['ID'] for x in actual }
   missing = { x for x in setStored if x not in setActual }
   for x in missing:
@@ -109,7 +117,7 @@ def down_nodes_add_from_list(stored, actual):
 def down_nodes_cleanup_by_timeout(endpoint, stored, timeout):
   log=logging.getLogger('pk_k8s')
   current_time = int(time.time())
-  for id, node in stored.items():
+  for id, node in stored.iteritems():
     if node['micado_timestamp']+timeout < current_time:
       log.info('(M)   => Node {0} is down for more than {1} seconds, removing.'.format(id,timeout))
       remove_node(endpoint,id)
